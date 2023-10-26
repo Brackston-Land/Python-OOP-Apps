2cd4217e1250709abc408ca3296ffd3d855cecb2
BrackstonLand <brackstonm@gmail.com>
2023-10-25 23:58:00 -0500

    Planning Phase - Added Class templates

--- App-2-Roommate-Bill-Sharing-App/files/main.py
+++ App-2-Roommate-Bill-Sharing-App/files/main.py
@@ -1,8 +1,40 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as total amount and period of the bill
+    """
 
-#App Is
-#
-# #Under Construction
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
 
-#test comment
-#test comment 2
-#test comment 3
+
+class Roommate:
+    """
+    Creates a roommate person who lives in the apartment and pays a share to the bill
+    """
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount / 2
+
+
+class PdfReport:
+    """
+    Create a pdf file that contains data about the roommates such as
+    their names, their due amount and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, roommate1, roommate2, bill):
+        pass
+
+
+the_bill = Bill(amount=120, period="March 2021")
+john = Roommate(name="John", days_in_house=20)
+marry = Roommate(name="Marry", days_in_house=25)
+
+print(john.pays(bill=the_bill))
